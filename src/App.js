68847a38427c5ff5f483b9af48dc697bb8126ccf
AymenKhanfir <aymenkhanfir56@gmail.com>
2023-02-26 01:00:47 +0100

    adding new file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import avatar from './avatar.png';
+
+function Logo(props) {
+  const userPic = <img src={avatar} alt="Coursera avatar" />;
+  return userPic;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello Word!</h1>
+      <Logo />
     </div>
   );
 }
